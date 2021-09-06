0ece2b9cf86a522bdea2ae18183e986805c759e0
twinklets <twinklets@669.com>
2021-09-06 14:36:53 +0530

    Initial commit

---
+++ linked list.py
@@ -0,0 +1,31 @@
+# llist = ['first', 'second', 'third']
+# print(llist)
+#
+# print()
+#
+# # adding elements
+# llist.append('fourth')
+# llist.append('fifth')
+# llist.insert(3, 'sixth')
+# print(llist)
+# print()
+#
+# llist.remove('second')
+# print(llist)
+# print()
+
+
+llist=[1,2,3,4,5,1]
+print(llist)
+print([id(i) for i in llist])
+llist.append(4)
+print(llist)
+llist.insert(4,2)
+print(llist)
+llist.remove(2)
+
+print(llist)
+
+list=[1,2,3,4,5,1]
+print(list)
+print([id(i) for i in list])
---
+++ main.py
@@ -0,0 +1,16 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+list=["12345-568","12345-567","12345-566","12358-564","12325-563"]
+a=[]
+
+dic={}
+for i in list:
+   b= i.split("-")[0]
+   a.append(b)
+   c=i.split("-")[1]
+
+print(dic)
+
